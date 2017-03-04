28bc5d0276237f5fc15f9d2ba6aeb08c494c768b
adamode <mode388@gmail.com>
2017-03-04 23:36:58 +0800

    First Commit

---
+++ pokedex3/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  pokedex3
+//
+//  Created by Mohd Adam on 04/03/2017.
+//  Copyright © 2017 Mohd Adam. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete  = () -> ()
---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Mohd Adam on 02/03/2017.
+//  Copyright © 2017 Mohd Adam. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        
+     super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
